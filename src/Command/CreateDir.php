418147a05c2f65958f37ea74a215f526279c2dba
runliu <m17865680858@a63.com>
2021-10-12 18:53:06 +0800

    one

---
+++ src/Command/CreateDir.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace AUV_devtool\Command;
+
+use Hyperf\Command\Annotation\Command;
+use Hyperf\Command\Command as HyperfCommand;
+use Psr\Container\ContainerInterface;
+
+/**
+ * @Command
+ */
+#[Command]
+class CreateDir extends HyperfCommand
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+
+        parent::__construct('auv:create_dir');
+    }
+
+    public function configure()
+    {
+        parent::configure();
+        $this->setDescription('Hyperf Demo Command');
+    }
+
+    public function handle(): void
+    {
+        $dirs = [
+            '/app/Service',
+            '/app/Controller',
+            '/app/Crontab',
+            '/app/Exception',
+            '/app/Model',
+            '/app/Processes',
+            '/app/Request',
+            '/app/Request',
+            '/app/Traits',
+            '/app/Utils',
+        ];
+
+        foreach ($dirs as $rawName) {
+            if ($this->alreadyExists($rawName)) {
+                $this->line(sprintf('目录已经存在：%s', $rawName), 'comment');
+                continue;
+            }
+            $path = $this->makeDirectory($rawName);
+            $this->line(sprintf('目录创建成功：%s', $path), 'info');
+        }
+    }
+
+    protected function alreadyExists(string $rawName): bool
+    {
+        return is_dir(BASE_PATH . $rawName);
+    }
+
+    protected function makeDirectory(string $rawName): string
+    {
+        mkdir(BASE_PATH . $rawName, 0777, true);
+        return $rawName;
+    }
+}
